0aa6a58d97441f863da5dd0d1d1d95903e95f6aa
WAnubhavGoel <goelanubhav70@gmail.com>
2026-04-02 13:47:24 +0000

    Connected with cloud database.

--- db/pool.cjs
+++ db/pool.cjs
@@ -1,4 +1,5 @@
 const {Pool} = require('pg');
+require('dotenv').config();
 
 module.exports = new Pool({
     connectionString: process.env.DB_CONNECTION_STRING,
--- models/AppData.cjs
+++ models/AppData.cjs
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const TITLE = process.env.TITLE || 'Mini Messaging Board';
 const PORT = process.env.PORT || 8080;
 
